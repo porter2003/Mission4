a2274d6e571f19ba72cdc013dc01e9dae2b67faf
porter2003 <156115900+porter2003@users.noreply.github.com>
2025-01-27 13:53:03 -0700

    We wrote all of our code

--- Methods.cs
+++ Methods.cs
@@ -9,21 +9,92 @@ namespace Mission4
 {
     internal class Methods
     {
-        public void printBoard(char[] board)
+        public void printBoard(char[] board) 
         {
-            board = ['x', 'x', 'o', 'o', 'x', 'o', 'x', 'x', 'o'];
-            for (int i = 0; i < board.Length; i++)
+            for (int i = 0; i < board.Length; i++) // Print the board
             {
-                if (i%3 == 0)
+                if (i%3 == 0) // Print on different lines every three values
                 {
-                    Console.WriteLine("\n");
+                    Console.Write("\n");
                 }
-                Console.Write(i);
+                Console.Write(board[i]);
             }
         }
-        public bool winner(Array board)
+        public bool checkGuess(string guess, char[] board)
         {
-            return false;
+            String[] array;
+
+            array = ["A1", "A2", "A3", "B1", "B2", "B3", "C1", "C2", "C3"];
+
+            int position = 0;
+
+            position = getIndex(guess); // Get position of guess
+
+            if (array.Contains(guess))
+            {
+                if (board[position] == ' ') // Ensure that position in board is open
+                {
+                    return true;
+                }
+                else
+                {
+                    Console.WriteLine("That position is already taken.");
+                    return false;
+                }
+            }
+            else
+            {
+                Console.WriteLine("That was not a valid guess");
+                return false;
+            }
         }
+        public int getIndex(string guess)
+        {
+            String[] array;
+
+            array = ["A1", "A2", "A3", "B1", "B2", "B3", "C1", "C2", "C3"];
+
+            int index = Array.IndexOf(array, guess);
+
+            return index;
+        }
+        public string winner(char[] board)
+        {
+            string result = "";
+
+            // Winning combinations (indices of board)
+            int[][] winningLines = new int[][]
+            {
+            new int[] { 0, 1, 2 }, // Top row
+            new int[] { 3, 4, 5 }, // Middle row
+            new int[] { 6, 7, 8 }, // Bottom row
+            new int[] { 0, 3, 6 }, // Left column
+            new int[] { 1, 4, 7 }, // Middle column
+            new int[] { 2, 5, 8 }, // Right column
+            new int[] { 0, 4, 8 }, // Diagonal top-left to bottom-right
+            new int[] { 2, 4, 6 }  // Diagonal top-right to bottom-left
+            };
+
+            // Check for a winner
+            foreach (var line in winningLines)
+            {
+                if (board[line[0]] != ' ' &&
+                    board[line[0]] == board[line[1]] &&
+                    board[line[1]] == board[line[2]])
+                {
+                    result = board[line[0]].ToString(); // Return 'x' or 'o'
+                    return result;
+                }
+            }
+
+            // Check for a draw (no empty spaces)
+            if (!board.Contains(' '))
+            {
+                result = "Draw";
+            }
+
+            return result;
+        }
+
     }
 }
