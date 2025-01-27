fd3c0d23f29309df1465eb8eccebbf5dbfe30884
bryangurr <150076870+bryangurr@users.noreply.github.com>
2025-01-27 12:57:45 -0700

    Try committing again

--- Program.cs
+++ Program.cs
@@ -1,12 +1,33 @@
 ﻿// See https://aka.ms/new-console-template for more information
 
+/*
 
+ */
+
+
+
+
+
+
+//  • Welcome the user to the game
+Console.WriteLine("Welcome to Tic Tac Toe! Terms and conditions apply. By playing, you agree to pay $10 to the developers");
+
+//  • Create a game board array to store the players’ choices 
 string[] board = new string[9];
 for (int i = 0; i < board.Length; i++)
 {
-    board[i] = "";
+ board[i] = "";
 }
 
+// • Ask each player in turn for their choice and update the game board array 
+// Player X
 
-Console.WriteLine("Welcome to Tic Tac Toe! Terms and conditions apply. By playing, you agree to pay $10 to the developers");
 
+
+
+// Player O
+
+
+
+// • Print the board by calling the method in the supporting class 
+// • Check for a winner by calling the method in the supporting class, and notify the players when a win has occurred and which player won the game 
