478ac8cf91d61b24739d65439f91edaba3e2d6a6
bryangurr <150076870+bryangurr@users.noreply.github.com>
2025-01-27 13:32:52 -0700

    Add game flow

--- Program.cs
+++ Program.cs
@@ -5,22 +5,57 @@
  */
 
 
-
-
-
+using static Mission4.Methods;
 
 //  • Welcome the user to the game
 Console.WriteLine("Welcome to Tic Tac Toe! Terms and conditions apply. By playing, you agree to pay $10 to the developers");
 // Changes
 //  • Create a game board array to store the players’ choices 
+string coordinates = "";
+string guess = "";
+string player = "";
+bool validGuess = false;
 string[] board = new string[9];
 for (int i = 0; i < board.Length; i++)
 {
  board[i] = "";
 }
+Mission4.Methods methods = new Mission4.Methods();
+
 
 // • Ask each player in turn for their choice and update the game board array 
 // Player X
+while (!winner(board))
+{   
+    validGuess = false;
+    while (validGuess == false)
+    {
+        // Console.Clear();
+        methods.printBoard(board);
+        Console.WriteLine("Enter coordinates: ");
+        coordinates = Console.ReadLine();
+        if (checkGuess(coordinates))
+        {
+            validGuess = true;
+
+            board[getIndex(coordinates)] = player;
+
+            if (player == "X")
+            {
+                player = "O";
+            }
+            else
+            {
+                player = "X";
+            }
+
+        }
+
+    }
+}
+
+
+
 
 
 
