939c33ef38b1d05b63ef0d9e5f5bb98317f7b16b
bryangurr <150076870+bryangurr@users.noreply.github.com>
2025-01-27 12:50:19 -0700

    Update and pull class

--- Program.cs
+++ Program.cs
@@ -1,2 +1,12 @@
 ﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+
+
+string[] board = new string[9];
+for (int i = 0; i < board.Length; i++)
+{
+    board[i] = "";
+}
+
+
+Console.WriteLine("Welcome to Tic Tac Toe! Terms and conditions apply. By playing, you agree to pay $10 to the developers");
+
